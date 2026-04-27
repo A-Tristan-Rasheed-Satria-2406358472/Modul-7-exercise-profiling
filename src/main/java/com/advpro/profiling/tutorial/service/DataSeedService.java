ceb1ce63c1e91c042fe39716d2f51754ded8a976
Cyaside <tristan.rasheedsatria@gmail.com>
2026-04-27 12:16:08 +0700

    chore : setup and also docker file

--- src/main/java/com/advpro/profiling/tutorial/service/DataSeedService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/DataSeedService.java
@@ -1,5 +1,13 @@
 package com.advpro.profiling.tutorial.service;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.advpro.profiling.tutorial.model.Course;
 import com.advpro.profiling.tutorial.model.Student;
 import com.advpro.profiling.tutorial.model.StudentCourse;
@@ -7,13 +15,6 @@ import com.advpro.profiling.tutorial.repository.CourseRepository;
 import com.advpro.profiling.tutorial.repository.StudentCourseRepository;
 import com.advpro.profiling.tutorial.repository.StudentRepository;
 import com.github.javafaker.Faker;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Locale;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 /**
  * @author muhammad.khadafi
@@ -21,59 +22,59 @@ import java.util.stream.Collectors;
 @Service
 public class DataSeedService {
 
-    @Autowired
-    private StudentRepository studentRepository;
-    @Autowired
-    private CourseRepository courseRepository;
-    @Autowired
-    private StudentCourseRepository studentCourseRepository;
+  @Autowired
+  private StudentRepository studentRepository;
+  @Autowired
+  private CourseRepository courseRepository;
+  @Autowired
+  private StudentCourseRepository studentCourseRepository;
 
-    private static final int NUMBER_OF_STUDENTS = 20_000;
-    private static final int NUMBER_OF_COURSE = 10;
+  private static final int NUMBER_OF_STUDENTS = 20_000;
+  private static final int NUMBER_OF_COURSE = 10;
 
-    public void seedStudent() {
-        Faker faker = new Faker(new Locale("in-ID"));
+  public void seedStudent() {
+    Faker faker = new Faker(new Locale("in-ID"));
 
-        for (int i = 0; i < NUMBER_OF_STUDENTS; i++) {
-            Student student = new Student();
-            student.setStudentCode(faker.code().ean8());
-            student.setName(faker.name().fullName());
-            student.setFaculty(faker.educator().course());
-            student.setGpa(faker.number().randomDouble(2, 2, 4));
+    for (int i = 0; i < NUMBER_OF_STUDENTS; i++) {
+      Student student = new Student();
+      student.setStudentCode(faker.code().ean8());
+      student.setName(faker.name().fullName());
+      student.setFaculty(faker.educator().course());
+      student.setGpa(faker.number().randomDouble(2, 2, 4));
 
-            studentRepository.save(student);
-        }
+      studentRepository.save(student);
+    }
+  }
+
+  public void seedCourse() {
+    Faker faker = new Faker(new Locale("in-ID"));
+    for (int i = 0; i < NUMBER_OF_COURSE; i++) {
+      Course course = new Course();
+      course.setCourseCode(faker.code().ean8());
+      course.setName(faker.book().title());
+      course.setDescription(faker.lorem().sentence());
+
+      courseRepository.save(course);
+    }
+  }
+
+  public void seedStudentCourses() {
+    List<Student> students = studentRepository.findAll();
+    List<Course> courses = courseRepository.findAll();
+
+    for (Student student : students) {
+      List<Course> selectedCourses = new Random().ints(0, courses.size())
+          .distinct()
+          .limit(2)
+          .mapToObj(courses::get)
+          .collect(Collectors.toList());
+
+      for (Course course : selectedCourses) {
+        StudentCourse studentCourse = new StudentCourse(student, course);
+        studentCourseRepository.save(studentCourse);
+      }
     }
 
-    public void seedCourse() {
-        Faker faker = new Faker(new Locale("in-ID"));
-        for (int i = 0; i < NUMBER_OF_COURSE; i++) {
-            Course course = new Course();
-            course.setCourseCode(faker.code().ean8());
-            course.setName(faker.book().title());
-            course.setDescription(faker.lorem().sentence());
-
-            courseRepository.save(course);
-        }
-    }
-
-    public void seedStudentCourses() {
-        List<Student> students = studentRepository.findAll();
-        List<Course> courses = courseRepository.findAll();
-
-        for (Student student : students) {
-            List<Course> selectedCourses = new Random().ints(0, courses.size())
-                    .distinct()
-                    .limit(2)
-                    .mapToObj(courses::get)
-                    .collect(Collectors.toList());
-
-            for (Course course : selectedCourses) {
-                StudentCourse studentCourse = new StudentCourse(student, course);
-                studentCourseRepository.save(studentCourse);
-            }
-        }
-
-    }
+  }
 
 }
